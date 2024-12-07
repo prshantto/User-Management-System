6eadfde6835ddfcce3b8a1d22e01f926796042b8
Prashant Singh <prashanttt001@gmail.com>
2024-12-07 13:42:00 +0530

    updated likehandler

--- frontend/src/components/UserCard.jsx
+++ frontend/src/components/UserCard.jsx
@@ -5,9 +5,11 @@ import {
   MailOutlined,
   PhoneOutlined,
   GlobalOutlined,
-  HeartTwoTone,
+  // HeartTwoTone,
   EditOutlined,
   DeleteFilled,
+  HeartOutlined,
+  HeartFilled,
 } from "@ant-design/icons";
 
 function ShowUser({ name, email, phone, website }) {
@@ -19,13 +21,28 @@ function ShowUser({ name, email, phone, website }) {
     return number < 10 ? `0${number}` : `${number}`;
   };
 
+  const likehandler = (event) => {
+    let Favourites = true;
+    const currentColor = event.target.style.color;
+    if(currentColor==='rgb(235, 47, 150)'){
+      event.target.style.color = ''
+      Favourites = false;
+      console.log(Favourites)
+    }else{
+      event.target.style.color = '#EB2F96'
+      Favourites = true;
+      console.log(Favourites)
+    }
+  }
+
+
   return (
     <>
       <div className=" border border-gray-500 mt-12 ml-12 mr-12 lg:m-2,mt-12 md:m-2 md:mt-12">
 
         <div className="h-50% bg-gray-100 text-center flex items-center justify-center">
           <img
-            className="rounded-full p-3 cursor-pointer"
+            className="rounded-full p-2 cursor-pointer"
             src={`https://api.dicebear.com/9.x/lorelei/svg?size=150&hair=variant${randomNumber()}&eyes=variant${randomNumber()}&mouth=happy${randomNumber()}&nose=variant04`}
             alt="Avatar"
           />
@@ -45,8 +62,10 @@ function ShowUser({ name, email, phone, website }) {
 
         </div>
 
-        <div className="h-10% bg-gray-200 flex items-center justify-around cursor-pointer border-t border-gray-500">
-          <HeartTwoTone twoToneColor={"#eb2f96"} />
+
+
+        <div className="h-10% bg-gray-200 flex items-center justify-around cursor-pointer border-t border-gray-400 text-gray-600">
+          <HeartFilled className="hover:text-[#EB2F96] " onClick={likehandler}/>
           <EditOutlined onClick={()=>setShowCard(true)} className="hover:text-blue-600" />
           {showCard && <UpdateUser onclose={()=>setShowCard(false)}/>}
           <DeleteFilled className="hover:text-blue-600" />
--- frontend/src/components/topbar.jsx
+++ frontend/src/components/topbar.jsx
@@ -6,9 +6,9 @@ function Topbar() {
 const [showCard,setShowCard] = useState(false)
     
   return (
-    <div className='w-full h-10 flex justify-end items-center gap-2 bg-gray-200 fixed'>
-        <button><HeartOutlined/> Favourites</button>
-        <PlusCircleOutlined style={{fontSize:"30px", cursor:"pointer"}} onClick={()=>setShowCard(true)} className='m-2'/>
+    <div className='w-full h-10 flex justify-end items-center gap-1 bg-gray-200 fixed rounded-b-lg'>
+        <button className='bg-[#fa5eb4c8] text-[12px] text-white font-semibold p-1.5 rounded-xl'><HeartOutlined/> Favourites</button>
+        <PlusCircleOutlined style={{fontSize:"25px", cursor:"pointer"}} onClick={()=>setShowCard(true)} className='m-2'/>
         {showCard && <CreateUser onclose={()=>setShowCard(false)}/>}
     </div>
   )
