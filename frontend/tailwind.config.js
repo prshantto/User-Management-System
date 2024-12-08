1d23f4a4c0900da1e490aaf5193da0973318f54c
Prashant Singh <prashanttt001@gmail.com>
2024-12-08 11:23:07 +0530

    debug

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,7 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */ 
+import plugin from 'tailwindcss/plugin'; 
+import tailwindcssAnimate from 'tailwindcss-animate';
+
 export default {
     darkMode: ["class"],
     content: [
