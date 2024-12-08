7671590c0e4c341a3e177ef5569dbd69ea50e009
Prashant Singh <prashanttt001@gmail.com>
2024-12-08 11:26:11 +0530

    debug

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,90 +1,87 @@
-/** @type {import('tailwindcss').Config} */ 
-import plugin from 'tailwindcss/plugin'; 
+/** @type {import('tailwindcss').Config} */
+import plugin from 'tailwindcss/plugin';
 import tailwindcssAnimate from 'tailwindcss-animate';
 
 export default {
-    darkMode: ["class"],
-    content: [
+  darkMode: ["class"],
+  content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-  	extend: {
-		height: { 
-			'60%': '60%',
-			'50%': '50%',
-			'40%': '40%' ,
-			'30%': '30%' ,
-			'10%': '10%'
-
-		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
-  		}
-  	}
+    extend: {
+      height: { 
+        '60%': '60%',
+        '50%': '50%',
+        '40%': '40%' ,
+        '30%': '30%' ,
+        '10%': '10%'
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)'
+      },
+      colors: {
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))'
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))'
+        },
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))'
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))'
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))'
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))'
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))'
+        },
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        chart: {
+          '1': 'hsl(var(--chart-1))',
+          '2': 'hsl(var(--chart-2))',
+          '3': 'hsl(var(--chart-3))',
+          '4': 'hsl(var(--chart-4))',
+          '5': 'hsl(var(--chart-5))'
+        }
+      }
+    }
   },
   plugins: [
-	require("tailwindcss-animate"),
-	function({ addUtilities }) { 
-		addUtilities({ 
-			'.no-spin': { 
-				'-moz-appearance': 'textfield', 
-				'-webkit-appearance': 'none', 
-				'margin': '0', 
-			}, 
-			'.no-spin::-webkit-inner-spin-button': { 
-				'-webkit-appearance': 'none', 
-			}, 
-			'.no-spin::-webkit-outer-spin-button': 
-			{ 
-				'-webkit-appearance': 'none', 
-			}, 
-		});
-	}
+    tailwindcssAnimate,
+    function({ addUtilities }) { 
+      addUtilities({ 
+        '.no-spin': { 
+          '-moz-appearance': 'textfield', 
+          '-webkit-appearance': 'none', 
+          'margin': '0', 
+        }, 
+        '.no-spin::-webkit-inner-spin-button': { 
+          '-webkit-appearance': 'none', 
+        }, 
+        '.no-spin::-webkit-outer-spin-button': { 
+          '-webkit-appearance': 'none', 
+        }, 
+      });
+    }
   ],
-}
-
+};
